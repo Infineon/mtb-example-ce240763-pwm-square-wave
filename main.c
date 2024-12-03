66aa4eecc6ba01e8efae8e18ee3ba318832693fe
gitlab-runner <do-not-reply@cypress.com>
2024-12-02 19:05:03 -0600

    Upload mtb-example-ce240763-pwm-square-wave [3010]

---
+++ main.c
@@ -0,0 +1,158 @@
+/*******************************************************************************
+* File Name:   main.c
+*
+* Description: This is the source code for the PWM square wave code example
+*              for ModusToolbox.
+*
+* Related Document: See README.md
+*
+*******************************************************************************
+* Copyright 2024, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+*
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
+
+/*******************************************************************************
+* Header Files
+*******************************************************************************/
+#include "cy_pdl.h"
+#include "cybsp.h"
+#include "cy_retarget_io.h"
+#include <inttypes.h>
+
+/*******************************************************************************
+* Function Definitions
+*******************************************************************************/
+/*******************************************************************************
+* Function Name: handle_error
+********************************************************************************
+* Summary:
+*  User defined error handling function.
+*
+* Parameters:
+*  status - status for evaluation.
+*
+* Return:
+*  void
+*
+*******************************************************************************/
+void handle_error(cy_rslt_t status)
+{
+    if (CY_RSLT_SUCCESS != status)
+    {
+        /* Halt the CPU while debugging */
+        CY_ASSERT(0);
+    }
+}
+
+/*******************************************************************************
+* Function Name: check_status
+********************************************************************************
+* Summary:
+*  Prints the message and waits forever when an error occurs.
+*
+* Parameters:
+*  message - message to print if status is non-zero.
+*  status - status for evaluation.
+*
+* Return:
+*  void
+*
+*******************************************************************************/
+void check_status(char *message, cy_rslt_t status)
+{
+    if (CY_RSLT_SUCCESS != status)
+    {
+        printf("\r\n=====================================================\r\n");
+        printf("\nFAIL: %s\r\n", message);
+        printf("Error Code: 0x%08" PRIX32 "\n", status);
+        printf("\r\n=====================================================\r\n");
+
+        while(true);
+    }
+}
+
+/*******************************************************************************
+* Function Name: main
+********************************************************************************
+* Summary:
+* This is the main function for the CPU. It configures the PWM and puts the CPU
+* in Sleep mode to save power.
+*
+* Parameters:
+*  void
+*
+* Return:
+*  int
+*
+*******************************************************************************/
+int main(void)
+{
+    /* API return code */
+    cy_rslt_t result;
+
+    /* Initialize the device and board peripherals */
+    result = cybsp_init();
+    handle_error(result);
+
+    /* Enable global interrupts */
+    __enable_irq();
+
+    /* Initialize retarget-io to use the debug UART port */
+    Cy_SCB_UART_Init(UART_HW, &UART_config, NULL);
+    Cy_SCB_UART_Enable(UART_HW);
+    result = cy_retarget_io_init(UART_HW);
+
+    /* If retarget-io init failed, it stops program execution */
+    handle_error(result);
+
+    printf("retarget-io ver1.6 testing \r\n");
+
+    /* \x1b[2J\x1b[;H - ANSI ESC sequence for clear screen */
+    printf("\x1b[2J\x1b[;H");
+    printf("******************** "
+           "PDL: PWM Square Wave "
+           "******************** \r\n\n");
+
+    /* In this example, PWM output is routed to the user LED on the kit. */
+    Cy_TCPWM_PWM_Init(TCPWM_PWM_HW, TCPWM_PWM_NUM, &TCPWM_PWM_config);
+    Cy_TCPWM_PWM_Enable(TCPWM_PWM_HW, TCPWM_PWM_NUM);
+    Cy_TCPWM_TriggerStart_Single(TCPWM_PWM_HW, TCPWM_PWM_NUM);
+
+    printf("PWM started successfully. Entering the sleep mode...\r\n");
+
+    for (;;)
+    {
+        /* Put the CPU into sleep mode to save power */
+        Cy_SysPm_CpuEnterSleep(CY_SYSPM_WAIT_FOR_INTERRUPT);
+    }
+}
+
+/* [] END OF FILE */
